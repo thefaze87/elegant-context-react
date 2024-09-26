f0f1f6b0bafefda09362ac330a271b54942cf398
Mark Fasel <markfasel@gmail.com>
2024-09-26 07:22:34 -0400

    destructure the content to items for shorter code

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,8 +2,8 @@ import { useContext } from 'react';
 import { CartContext } from '../store/shopping-cart-context';
 
 export default function Cart({ onUpdateItemQuantity }) {
-    const cartCtx = useContext(CartContext);
-    const totalPrice = cartCtx.items.reduce(
+    const { items } = useContext(CartContext);
+    const totalPrice = items.reduce(
         (acc, item) => acc + item.price * item.quantity,
         0
     );
@@ -11,10 +11,10 @@ export default function Cart({ onUpdateItemQuantity }) {
 
     return (
         <div id="cart">
-            {cartCtx.items.length === 0 && <p>No items in cart!</p>}
-            {cartCtx.items.length > 0 && (
+            {items.length === 0 && <p>No items in cart!</p>}
+            {items.length > 0 && (
                 <ul id="cart-items">
-                    {cartCtx.items.map((item) => {
+                    {items.map((item) => {
                         const formattedPrice = `$${item.price.toFixed(2)}`;
 
                         return (
