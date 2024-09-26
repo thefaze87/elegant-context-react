2ee2599fe9870fe1b088715218b62cb4f8ed8d77
Mark Fasel <markfasel@gmail.com>
2024-09-26 08:03:29 -0400

    updated app to use context api and remove prop drilling entirely

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,8 @@
 import { useContext } from 'react';
 import { CartContext } from '../store/shopping-cart-context';
 
-export default function Cart({ onUpdateItemQuantity }) {
-    const { items } = useContext(CartContext);
+export default function Cart() {
+    const { items, updateItemQuantity } = useContext(CartContext);
     const totalPrice = items.reduce(
         (acc, item) => acc + item.price * item.quantity,
         0
@@ -26,7 +26,7 @@ export default function Cart({ onUpdateItemQuantity }) {
                                 <div className="cart-item-actions">
                                     <button
                                         onClick={() =>
-                                            onUpdateItemQuantity(item.id, -1)
+                                            updateItemQuantity(item.id, -1)
                                         }
                                     >
                                         -
@@ -34,7 +34,7 @@ export default function Cart({ onUpdateItemQuantity }) {
                                     <span>{item.quantity}</span>
                                     <button
                                         onClick={() =>
-                                            onUpdateItemQuantity(item.id, 1)
+                                            updateItemQuantity(item.id, 1)
                                         }
                                     >
                                         +
